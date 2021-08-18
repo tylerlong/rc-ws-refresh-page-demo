dcd00d4c599b09f7af66716c77f3bbd91da20173
Tyler Long <tyler4long@gmail.com>
2021-08-18 11:46:49 -0700

    Restore subscriptions

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 import RingCentral from '@rc-ex/core';
 import WebSocketExtension, {Events} from '@rc-ex/ws';
 import localforage from 'localforage';
-import {TokenInfo} from '@rc-ex/core/lib/definitions';
+import {SubscriptionInfo, TokenInfo} from '@rc-ex/core/lib/definitions';
 import {WebSocketOptions, Wsc} from '@rc-ex/ws/lib/types';
-import waitFor from 'wait-for-async';
 
 const tokenKey = 'rc-ws-refresh-page-demo-token';
 const wscTokenKey = 'rc-ws-refresh-page-demo-wsc-token';
+const subscriptionKey = 'rc-ws-refresh-page-demo-subscription';
 
 const rc = new RingCentral({
   clientId: process.env.RINGCENTRAL_CLIENT_ID!,
   clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET!,
   server: process.env.RINGCENTRAL_SERVER_URL!,
 });
+
+const callback = (event: {}) => {
+  console.log(JSON.stringify(event, null, 2));
+};
 
 (async () => {
   // restore access token
@@ -34,28 +38,27 @@ const rc = new RingCentral({
   const wscToken = (await localforage.getItem(wscTokenKey)) as string;
   const webSocketExtensionOptions: WebSocketOptions = {};
   if (wscToken !== null) {
-    console.log('bingo', wscToken);
     webSocketExtensionOptions.wscToken = wscToken;
   }
   const webSocketExtension = new WebSocketExtension(webSocketExtensionOptions);
   webSocketExtension.eventEmitter.on(Events.newWsc, async (wsc: Wsc) => {
-    console.log('setItem');
     await localforage.setItem(wscTokenKey, wsc.token);
   });
   await rc.installExtension(webSocketExtension);
 
-  let count = 0;
-  if (wscToken === null) {
-    await webSocketExtension.subscribe(
-      ['/restapi/v1.0/account/~/extension/~/message-store'],
-      event => {
-        console.log(JSON.stringify(event, null, 2));
-        count += 1;
-      }
-    );
-  }
-  const r = await rc.restapi().subscription().list();
-  console.log(JSON.stringify(r, null, 2));
+  // restore subscription
+  const subscriptionInfo = (await localforage.getItem(
+    subscriptionKey
+  )) as SubscriptionInfo;
+  const subscription = await webSocketExtension.subscribe(
+    ['/restapi/v1.0/account/~/extension/~/message-store'],
+    event => callback(event),
+    subscriptionInfo // restore old one. create new one instead if this parameter is undefined or null
+  );
+  await localforage.setItem(subscriptionKey, subscription.subscriptionInfo);
+
+  const list = await rc.restapi().subscription().list();
+  console.log(JSON.stringify(list, null, 2));
 
   await rc
     .restapi()
@@ -67,6 +70,4 @@ const rc = new RingCentral({
       to: [{extensionNumber: '101'}], // send pager to oneself
       text: 'Hello world',
     });
-
-  await waitFor({condition: () => count > 0});
 })();
